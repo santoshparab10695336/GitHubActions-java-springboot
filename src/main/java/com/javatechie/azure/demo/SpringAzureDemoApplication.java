c240159981d7193d60698dce909165426bd8a9f3
santoshparab10695336 <119646316+santoshparab10695336@users.noreply.github.com>
2022-12-21 19:02:14 +0530

    Update SpringAzureDemoApplication.java

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. !";
+		return "Hello Santosh Parab. Welcome to LTIMindtree. Good to see you here. Have great future ahead!!!";
 	}
 
 	public static void main(String[] args) {
